24e302d089b3af8a2dae3d736bcd343fe9d2f852
hocaoglumf <hocaoglumf@gmail.com>
2025-02-12 17:04:13 +0300

    First Commit

---
+++ VeriTipleri/VeriTipleri.py
@@ -0,0 +1,18 @@
+# Tamsayı
+x=2
+#float
+y=2.0
+#bool
+b=True
+
+#complex
+cmp=type(2+3j)
+print(cmp)
+print(complex(2,5))
+print (complex(2,3)+complex(5,6))
+#string
+c="Merhaba"
+#Liste
+l=[1,2,3,4]
+# Sözlük
+s={"a":1,"b":2,"c":3}
